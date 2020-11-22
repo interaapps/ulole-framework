56d0d2de95ee4bf3838e98da730836ae742e8e0b
Julian Gojani <julian@gojani.xyz>
2020-11-22 19:07:32 +0100

    Added example factory

--- app/model/User.php
+++ app/model/User.php
@@ -2,11 +2,13 @@
 namespace app\model;
 
 use de\interaapps\ulole\core\traits\JSONModel;
+use de\interaapps\ulole\orm\ORMHelper;
 use de\interaapps\ulole\orm\ORMModel;
 
 class User {
     
-    use ORMModel; 
+    use ORMModel;
+    use ORMHelper; // Adds more ORM-Helper like User::all() and User::get(id)
     use JSONModel; // Adds $user->json() and User::fromJson("{...}")
 
     protected $ormSettings = [
@@ -14,5 +16,12 @@ class User {
     ];
     
     public $id,
-           $name;
+           $name,
+           $password,
+           $gender,
+           $created_at;
+
+    public function setPassword($password){
+        $this->password = password_hash($password, PASSWORD_BCRYPT);
+    }
 }
--- resources/migrations/migration_201122_001143_create_user.php
+++ resources/migrations/migration_201122_001143_create_user.php
@@ -1,6 +1,7 @@
 <?php
 namespace resources\migrations;
 
+use app\helper\factories\UserFactory;
 use de\interaapps\ulole\orm\Database;
 use de\interaapps\ulole\orm\migration\Blueprint;
 use de\interaapps\ulole\orm\migration\Migration;
@@ -10,17 +11,21 @@ use de\interaapps\ulole\orm\migration\Migration;
  */
 class migration_201122_001143_create_user implements Migration {
     public function up(Database $database) {
-        return $database->create("users", function (Blueprint $blueprint) {
+        $creationResult = $database->create("users", function (Blueprint $blueprint) {
             $blueprint->id();
-            $blueprint->string("name");
-            $blueprint->string("password");
+            $blueprint->string("name")->nullable(true);
+            $blueprint->string("password")->nullable(true);
             $blueprint->enum("gender", ["FEMALE", "MALE", "OTHER", "NO_ANSWER"])->default('NO_ANSWER');
             $blueprint->timestamp("created_at")->currentTimestamp();
         });
+
+        // Creating some example users
+        UserFactory::run(25);
+
+        return $creationResult;
     }
 
     public function down(Database $database) {
         return $database->drop("users");
-        
     }
 }
